82355a1518b767ff541c54db3299c8e325336bff
Smalsusanu1 <114911362+Smalsusanu1@users.noreply.github.com>
2025-02-27 16:00:03 +0530

    Update HelloWorld.tsx

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
 import type { IHelloWorldProps } from './IHelloWorldProps';
-import { escape } from '@microsoft/sp-lodash-subset';
 
 export default class HelloWorld extends React.Component<IHelloWorldProps> {
   public render(): React.ReactElement<IHelloWorldProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
 
     return (
       <> <h1> Hello Vivek</h1></>
